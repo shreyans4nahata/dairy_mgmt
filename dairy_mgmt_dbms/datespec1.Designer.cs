76193c8be7e706dd4716817c7c40fec70821b22f
unknown <shreyans4nahata@github.com>
2015-11-29 19:30:23 +0530

    between dates info

--- dairy_mgmt_dbms/datespec1.Designer.cs
+++ dairy_mgmt_dbms/datespec1.Designer.cs
@@ -33,6 +33,9 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,14 +58,14 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(15, 69);
+            this.dataGridView1.Location = new System.Drawing.Point(4, 69);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(412, 149);
+            this.dataGridView1.Size = new System.Drawing.Size(452, 149);
             this.dataGridView1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(15, 248);
+            this.button1.Location = new System.Drawing.Point(352, 251);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
@@ -72,7 +75,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(352, 248);
+            this.button2.Location = new System.Drawing.Point(15, 251);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 4;
@@ -80,11 +83,40 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(157, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 19);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Between Dates";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(107, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Date 2";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(157, 228);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 7;
+            // 
             // datespec1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 297);
+            this.ClientSize = new System.Drawing.Size(460, 297);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
@@ -105,5 +137,8 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
     }
 }
--- dairy_mgmt_dbms/datespec1.cs
+++ dairy_mgmt_dbms/datespec1.cs
@@ -64,5 +64,46 @@ namespace dairy_mgmt_dbms
             }
             
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cs = @"server=localhost;userid=root;password=1234;database=dairy_mgmt";
+                Console.WriteLine("inside");
+                MySqlConnection conn = null;
+                try
+                {
+                    conn = new MySqlConnection(cs);
+                    conn.Open();
+                    Console.WriteLine("MySQL version : {0}", conn.ServerVersion);
+                    string date1,date2;
+                    //to show data
+                    date1 = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+                    date2 = dateTimePicker2.Value.ToString("yyyy-MM-dd");
+                    Console.WriteLine("Date1 : {0}", date1);
+                    Console.WriteLine("Date2 : {0}", date2);
+                    MySqlDataAdapter da = new MySqlDataAdapter("SELECT * FROM milk_info WHERE milk_info.DOC BETWEEN '" + date1 + "' AND '"+ date2 +"' ;", conn);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    dataGridView1.DataSource = ds.Tables[0];
+                }
+                catch (MySqlException ex)
+                {
+                    Console.WriteLine("Error: {0}", ex.ToString());
+                }
+                finally
+                {
+                    if (conn != null)
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
     }
 }
